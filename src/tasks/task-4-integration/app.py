cbd27ba4975263dbe4a05078d1f4abbd694f1ca8
Ernest Offor Ugwoke <ernestoffor@gmail.com>
2022-01-17 00:16:20 +0100

    improve dashboard

--- src/tasks/task-4-integration/app.py
+++ src/tasks/task-4-integration/app.py
@@ -1,11 +1,17 @@
+from turtle import title
 import pandas as pd
 import numpy as np
 from sqlalchemy import create_engine
 import streamlit as st
-from water_recommendation import closest_taps
+from water_recommendation_distance import closest_taps_distance, getlocation
 import altair as alt
+import geocoder
+from geopy.geocoders import Nominatim
+import plotly.express as px
 
-st.title("A Web App For Getting Water Points Recommendations in Osun State, Nigeria")
+
+st.set_page_config(layout="wide")
+st.title("Explore Water Points in Nigeria")
 
 @st.cache
 def load_data(state):
@@ -19,27 +25,101 @@ def load_data(state):
     
     return df_state
 
+state = 'Osun'
 
-osun = load_data('Osun')
-
-taps = closest_taps(osun)
-
-st.write(taps)
-
-st.subheader("The Ten Nearest Water Points Distances")
-
-#st.bar_chart(taps['dist'])
+state1 = st.sidebar.selectbox("Select Your State of Interest: ", ("Abia", "Adamawa", \
+      "Akwa Ibom", "Anambra", "Bauchi", "Bayelsa", "Benue", "Borno", "Cross River",\
+           "Delta", "Ebonyi", "Edo", "Enugu", "Ekiti", "Gombe", "Imo", "Jigawa", "Kaduna",\
+                "Kano", "Katsina", "Kebbi", "Kogi", "Kwara", "Lagos", "Nasarawa", "Niger", \
+               "Ogun", "Osun", "Ondo", "Plateau", "Rivers", "Sokoto", "Taraba", \
+               "Yobe", "Zamfara"))
 
 
-c = alt.Chart(taps).mark_bar().encode(
-     x='location', y='dist',  color='location', tooltip=['location', 'population', 'dist'])
 
-st.altair_chart(c, use_container_width=True)
+df = load_data(state1)
+col1, col2, col3 = st.columns(3)
 
+with col1:
+    st.subheader("Status of Water in {}".format(state1))
+    status = df['status'].value_counts()[:4]
+    df_s = pd.DataFrame({"Frequency": status.values}, index = status.index)
+    df_s['status'] = df_s.index
+    fig = px.bar(df_s, y="Frequency", x="status", color="status", title="Status of Water in {}".format(state1))
+    st.plotly_chart(fig)
+    #st.bar_chart(df['status'].value_counts()[:4])
 
-st.subheader("The Ten Nearest Water Points With the population they serve")
+with col2:
+    st.caption("Map of Water Points in {}".format(state1))
+    st.map(df)
 
-d = alt.Chart(taps).mark_bar().encode(
-     x='location', y='population',  color='location', tooltip=['location', 'population', 'dist'])
+with col3:
+    quality = df['subjective_quality'].value_counts()
+    df_q = pd.DataFrame({"counts": quality.values}, index = quality.index)
+    df_q.index.names = ["quality"]
+    df_q['quality'] = df_q.index
+    fig = px.pie(df_q, values='counts', names='quality', title = "The quality of Water in {}".format(state1))
+    st.plotly_chart(fig)
 
-st.altair_chart(d, use_container_width=True)
+#df_q.plot.pie(y = 'counts', figsize=(6,8))
+dist = st.slider('Select a distance',0, 20, 8)
+st.write("Distance: ", dist)
+
+no_taps = st.slider('Select a Number of taps',0, 20, 8)
+st.write("Number of taps: ", no_taps)
+
+#Radio button to select location
+loc = st.radio(
+    "Select your prefered location",
+    ('Current Location','{} Location'.format(state1),"Select"))
+
+#Instantiating geolocator
+geolocator = Nominatim(user_agent="app1")
+
+if loc == "Current Location":
+    g = geocoder.ip('me')
+    latlng = g.latlng
+    lat = latlng[0]
+    lng = latlng[1]
+elif loc == "{} Location".format(state1):
+    #geolocator = Nominatim(user_agent="app1")
+    location = geolocator.geocode(state1)
+    lat = location.latitude
+    lng = location.longitude
+elif loc == "Select":
+    selloc = st.selectbox(
+     'How would you like to be contacted?',
+     getlocation())
+    location = geolocator.geocode(selloc)
+    
+    if location == None:
+        location = geolocator.geocode(state1)
+        lat = location.latitude
+        lng = location.longitude
+    else:
+        lat = location.latitude
+        lng = location.longitude
+        # if lat and long are not in range constrain to default range of osun region
+        if (float(lat) < float('7.060309')) or (float(lat) > float('8.061898')) or (float(lng) < float('4.032004')) or (float(lng) > float('5.055003')):
+            location = geolocator.geocode(state1)
+            lat = location.latitude
+            lng = location.longitude
+
+#display latitude and longitude
+st.write("Latitude: {}, Longitude {}".format(lat, lng))
+
+#Submit button to confirm distance, No of taps and Region
+btn = st.button("Submit")
+
+#find all the taps near by
+taps = closest_taps_distance(df,lat,lng,dist, no_taps)
+
+if btn:
+    st.subheader("The {} Nearest Water Points Distances".format(no_taps))
+
+    c = alt.Chart(taps).mark_bar().encode(
+        x='location', y='dist',  color='location', tooltip=['location', 'population', 'dist'])
+
+    st.altair_chart(c, use_container_width=True)
+    
+    #Display Data frame
+    st.dataframe(taps)
--- src/tasks/task-4-integration/water_recommendation.py
+++ src/tasks/task-4-integration/water_recommendation.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import numpy as np
 from sqlalchemy import create_engine
-
+import sqlite3 
 
 # Create an engine to connect to sqlite database
 engine = create_engine('sqlite:///water_points.db')
 conn = engine.connect()
+#conn = mysql.connector.connect('C:/Users/2039845/OneDrive - Cognizant/Desktop/Streamlit/Osun/water_points.db')
 # Load the dataset from the database
 df = pd.read_sql_table('etlTable', con=conn)
-
 
 def distance_between_two_points(lat1, lat2, lon1, lon2):
 
@@ -19,32 +19,24 @@ def distance_between_two_points(lat1, lat2, lon1, lon2):
         lat2: -> latitude of the second point in degree 
         lon1: -> longitude of the first point in degree 
         lon2: -> longitude of the second point in degree 
-     """    
+     """
+     
     # The numpy module contains a function named
-    # radians which converts from degrees to radians.
-
+    # radians which converts from degrees to radians
      lat1a = np.radians(lat1)
      lat2a = np.radians(lat2)
       
     # Haversine formula
-    
      dlon = np.radians(lon2 - lon1)
      dlat = np.radians(lat2 - lat1)
      a = np.sin(dlat / 2)**2 + np.cos(lat1a) * np.cos(lat2a) * np.sin(dlon / 2)**2
- 
-    
      c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a))
      r = 6371
       
      # calculate the result
-
      return np.round(c * r, 6)
 
-# Request the user's latitude and longitude
-
-
-
-def closest_taps(df1, num_taps=10):
+def closest_taps_distance(df1,lat, lng, dist, num_taps=10):
     """
     A procedure that recommends nearest water taps based on distance.
     INPUTS:
@@ -58,19 +50,21 @@ def closest_taps(df1, num_taps=10):
      #Create a column for distance between a household and nearest water points
     df = df1.copy()
     distance = []
-    lat1 = float(input("Enter your latitude in the range 7.060309 - 8.061898 and press enter:    "))
-    lon1 = float(input("Enter your longitude in the range 4.032004 - 5.055003 and press enter:   "))
+    lat1 = float(lat)
+    lon1 = float(lng)
     for row in df.itertuples(index=False):
         distance.append(distance_between_two_points(lat1, row.latitude, lon1, row.longitude) )
     
     df['dist'] = distance
     df = df[(df.subjective_quality=="Acceptable quality") & (df.status=="Functional (and in use)") & (df.water_source=="Borehole")]
     closest_taps = df.sort_values(by="dist")
+    
     # Get the top points
     closest_taps = closest_taps[['location', 'population', 'dist']][:num_taps]
     closest_taps= closest_taps.reset_index()
-    return closest_taps
-    
+    df1 = closest_taps.loc[closest_taps['dist']<= dist]
+    return df1
 
-#taps = closest_taps(df[df.state=='Osun'])
-#print(taps)
+def getlocation():
+    df2 = df.copy()
+    return df2["location"].unique().tolist()
